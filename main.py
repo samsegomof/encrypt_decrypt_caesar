47a280c13734f7c8eb214b545362007e735e87d9
samsegomof <wemagonof91@gmail.com>
2023-04-16 18:03:58 +0500

    Add docstrings and comments, update README.md and CHANGELOG.md

--- main.py
+++ main.py
@@ -3,26 +3,47 @@ from typing import Optional
 from validator import validate_params
 
 
-def encrypting(text: str, shift: int) -> Optional[str]:
+def encrypting(text: str, shift: int) -> str:
+    """
+    Функция шифрования методом шифра Цезаря
+    :param text: исходная строка для шифрования
+    :type text: str
+    :param shift: значение смещения для шифрования
+    :type shift: int
+    :return: зашифрованная строка
+    :rtype: str
+    """
     try:
-        validate_params(text, shift)
-        encrypt_text = []
+        validate_params(text, shift)  # перед тем как зашифровать текст проходит все проверки с помощью валидатора
+        encrypt_text = []  # пустой список для зашифрованного текста
         for symbol in text:
-            if symbol.isalpha():
-                if symbol.islower():
+            if symbol.isalpha():  # проверяем, является ли символ буквой
+                if symbol.islower():  # проверяем регистр символа
+                    # переводим буквенные символы в значения unicode, отнимаем значение unicode буквы "a",
+                    # прибавляем смещение, берем остаток от деления на 26 и прибавляем значение unicode буквы "a"
                     shifted_symbol = chr((ord(symbol) - ord("a") + shift) % 26 + ord("a"))
                 else:
+                    # аналогично для заглавных букв
                     shifted_symbol = chr((ord(symbol) - ord("A") + shift) % 26 + ord("A"))
-                encrypt_text.append(shifted_symbol)
+                encrypt_text.append(shifted_symbol)  # добавляем текст с учетом сдвига в список
 
             else:
-                encrypt_text.append(symbol)
-        return "".join(encrypt_text)
+                encrypt_text.append(symbol)  # иначе добавляем символ как есть
+        return "".join(encrypt_text)  # объединяем текст из списка в строку и возвращаем ее
     except (ValueError, TypeError) as e:
         return f"Error: {e}"
 
 
 def decrypting(text: str, shift: int) -> str:
+    """
+    Функция дешифрования методом шифра Цезаря
+    :param text: исходная строка для дешифрования
+    :type text: str
+    :param shift: значение смещения для дешифрования
+    :type shift: int
+    :return: дешифрованная строка
+    :rtype: str
+    """
     try:
         validate_params(text, shift)
 
@@ -31,6 +52,7 @@ def decrypting(text: str, shift: int) -> str:
         for symbol in text:
             if symbol.isalpha():
                 if symbol.islower():
+                    # при дешифровании - отнимаем смещение
                     shifted_symbol = chr((ord(symbol) - ord("a") - shift) % 26 + ord("a"))
                 else:
                     shifted_symbol = chr((ord(symbol) - ord("A") - shift) % 26 + ord("A"))
--- validator.py
+++ validator.py
@@ -12,20 +12,20 @@ def validate_params(text: str, shift: int) -> Any:
     :rtype shift: int
     :raises: TypeError, ValueError
     """
-    if not isinstance(text, str):
+    if not isinstance(text, str):  # проверка типа аргумента text
         raise TypeError("Wrong type of text")
-    if not isinstance(shift, int):
+    if not isinstance(shift, int):  # проверка типа аргумента shift
         raise TypeError("Wrong type of shift")
-    if shift < 0 or shift > 25:
+    if shift < 0 or shift > 25:  # проверка значения shift - должно быть в диапазоне от 0 до 25
         raise ValueError("Shift should be between 0 and 25")
-    if text == "":
+    if text == "":  # проверка на пустой text
         raise ValueError("Text should be not empty")
-    if not text.strip():
+    if not text.strip():  # проверка на переданные в поле text только пробелы
         raise ValueError("Text should not contain only spaces")
     for char in text:
-        if char.isalpha() and "а" <= char <= "я" or "А" <= char <= "Я":
+        if char.isalpha() and "а" <= char <= "я" or "А" <= char <= "Я":  # проверка на кириллицу
             raise ValueError("Cyrillic characters does not supported")
-        if char in SPEC_CHARS:
+        if char in SPEC_CHARS:  # проверка на введенные спецсимволы
             raise(ValueError("Special characters does not supported"))
-        if char.isdigit():
+        if char.isdigit():  # проверка на введенные цифры
             raise ValueError("Digits does not supported")
