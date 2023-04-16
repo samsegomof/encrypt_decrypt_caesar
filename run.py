fd89824c8d065e84023a7ed52ac806d61b0f2792
samsegomof <wemagonof91@gmail.com>
2023-04-16 17:02:09 +0500

    Added the ability to select a mode in a run.py and complete the execution with stop or exit command

--- run.py
+++ run.py
@@ -4,7 +4,10 @@ from main import encrypting, decrypting
 def run():
     while True:
         print("Добро пожаловать в шифратор/дешифратор методом Цезаря!")
-        print("Выберете режим:\nE - шифрования\nD - дешифрования")
+        print("Выберете режим:\n"
+              "E - шифрования\n"
+              "D - дешифрования\n"
+              "Exit, exit, - для завершения\n")
         choose = input()
         if choose == "E" or choose == "e":
             try:
@@ -14,7 +17,7 @@ def run():
                 print(f"Зашифрованный текст: {encrypted_text}")
             except Exception as e:
                 return e
-        if choose == "D" or choose == "d":
+        elif choose == "D" or choose == "d":
             try:
                 text = input("Введите текст для дешифрования:\n")
                 shift = int(input("Введите значение шага для дешифрования:\n"))
@@ -22,6 +25,10 @@ def run():
                 print(f"Дешифрованный текст: {decrypted_text}")
             except Exception as e:
                 return e
+
+        elif choose in ["exit", "Exit"]:
+            print("До новых встреч!")
+            break
 
 
 if __name__ == "__main__":
